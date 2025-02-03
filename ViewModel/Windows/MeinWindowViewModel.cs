f448cc0b864ff25d860aad405b5324bb22bd207f
Artem Pop <vip.artem.popov.14@mail.ru>
2025-02-03 20:37:02 +0500

    ViewModelBase add, creating viewModel for Ranking view

---
+++ ViewModel/Windows/MeinWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ModernSort.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace ModernSort.ViewModel
+{
+    class MeinWindowViewModel : ViewModelBase
+    {
+        public MeinWindowViewModel()
+        {
+            CloseApplicationCommand = new ApplicationCloseCommand();
+            CollapselicationCommand = new CollapseApplicationCommand();
+        }
+        public ICommand CloseApplicationCommand { get; }
+        public ICommand CollapselicationCommand { get; }
+    }
+}

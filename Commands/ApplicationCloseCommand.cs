ebecb25d69eb5c3acf97367993655d4a7c867867
Artem Pop <vip.artem.popov.14@mail.ru>
2025-02-03 20:29:53 +0500

    CommandBase, Collapse and Close Application Commands add

---
+++ Commands/ApplicationCloseCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernSort.Commands
+{
+
+    internal class ApplicationCloseCommand : CommandBase
+    {
+        public ApplicationCloseCommand() { }
+
+        public override void Execute(object? parameter)
+        {
+            CloseApplication();
+        /// <summary>
+        /// Метод, предназначенный для штатного завершения работы приложения
+        /// </summary>
+            void CloseApplication()
+            {
+                Environment.Exit(0);
+            }
+        }
+    }   
+}
+

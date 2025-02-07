84dc0930abe661740108ec37ed37094de34f714b
Artem Pop <vip.artem.popov.14@mail.ru>
2025-02-07 20:50:41 +0500

    Add new navigationCommand for testing

---
+++ Commands/NavigationCommand.cs
@@ -0,0 +1,31 @@
+﻿using ModernSort.Stores;
+using ModernSort.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernSort.Commands
+{
+    internal class NavigationCommand : CommandBase
+    {
+        private readonly NavigationStore _navigationStore;
+        private readonly Func<ViewModelBase> _createViewModel;
+
+        public NavigationCommand(NavigationStore navigationStore, Func<ViewModelBase> CreateViewModel)
+        {
+            _navigationStore = navigationStore;
+            this._createViewModel = CreateViewModel;
+        }
+
+        public override void Execute(object parameter)
+        {
+            ViewModelBase localViewModel = _createViewModel();
+            if (_navigationStore.CurrentViewModel.GetType() != localViewModel.GetType())
+            {
+                _navigationStore.CurrentViewModel = localViewModel;
+            }
+        }
+    }
+}
---
+++ Commands/OpenNewWindowCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernSort.Commands
+{
+    internal class OpenNewWindowCommand : CommandBase
+    {
+        private readonly Action _action;
+        public OpenNewWindowCommand(Action action)
+        {
+            _action = action;
+        }
+        public override void Execute(object? parameter)
+        {
+            _action?.Invoke();
+        }
+    }
+}

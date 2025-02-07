09bc141a56c60d1add16af822d81615ff842913f
Artem Pop <vip.artem.popov.14@mail.ru>
2025-02-07 12:11:33 +0500

    Deserializer usage on MeinWindowViewModel client part refactoring

--- ViewModel/Windows/MeinWindowViewModel.cs
+++ ViewModel/Windows/MeinWindowViewModel.cs
@@ -16,36 +16,37 @@ namespace ModernSort.ViewModel
 {
     class MeinWindowViewModel : ViewModelBase
     {
-        private IDeserializer _JsonDeserializer;
-        private ISerializer _jsonSerializer;
+        private IDeserializer _Deserializer {  get; init; }
+        private ISerializer _Serializer { get; init; }
         private IoCollection<RankingCategory> _rankingCategories;
         public ObservableCollection<RankingCategoryItemViewModel> RankingCategoriesItems
         { 
             get
             { 
-                return ParseListToCollection(_rankingCategories);
+                return ParseIoToCollection(_rankingCategories);
             } 
         }
 
         public MeinWindowViewModel(IDeserializer deserializer,ISerializer serializer)
         {
-            _JsonDeserializer = deserializer;
-            _jsonSerializer = serializer;
-            _rankingCategories = deserializer.DeserializeList<RankingCategory>(@"1.json"); ////// Исправить Рефакторинг интерфейсов IDeSerializable!!!
-            //_rankingCategories = new List<RankingCategory>()
-            //{
-            //    new RankingCategory() {ID = 1,Tytle = "Название1",Description = "Описание1",RankingDirrectoryPath = "Путь1", RankingIconPath = "UserResources\\RankingCategory_1\\Ranking_Icon.jpg"},
-            //    new RankingCategory() {ID = 2,Tytle = "Название2",Description = "Описание2",RankingDirrectoryPath = "Путь2", RankingIconPath = "UserResources\\RankingCategory_2\\Ranking_Icon.jpg"},
-            //    new RankingCategory() {ID = 3,Tytle = "Название3",Description = "Описание3",RankingDirrectoryPath = "Путь3",RankingIconPath = "UserResources\\RankingCategory_3\\Ranking_Icon.jpg"},
-            //    new RankingCategory() {ID = 4, Tytle = "Название4",Description = "Описание4", RankingDirrectoryPath = "Путь4",RankingIconPath = "UserResources\\RankingCategory_4\\Ranking_Icon.jpg" }
-            //};
+            _Deserializer = deserializer;
+            _Serializer = serializer;
+
+            _rankingCategories = new IoCollection<RankingCategory>();
+            _rankingCategories.Deserialize(deserializer, @"RankingCategories.json");
+
             CloseApplicationCommand = new ApplicationCloseCommand();
             CollapselicationCommand = new CollapseApplicationCommand();
         }
         public ICommand CloseApplicationCommand { get; }
         public ICommand CollapselicationCommand { get; }
 
-        private ObservableCollection<RankingCategoryItemViewModel> ParseListToCollection(IEnumerable<RankingCategory> list)
+        /// <summary>
+        /// Метод преобразует IoCollection в ObservableCollection
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        private ObservableCollection<RankingCategoryItemViewModel> ParseIoToCollection(IEnumerable<RankingCategory> list)
         {
             return new ObservableCollection<RankingCategoryItemViewModel>(list.Select(x => new RankingCategoryItemViewModel(x)));
         }

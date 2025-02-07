cc00faffe6ce4fa8d699f448305ce0c412cf7783
Artem Pop <vip.artem.popov.14@mail.ru>
2025-02-07 20:52:18 +0500

    Add static ProjactIoWorker.cs class for working with Io elements

---
+++ Static/ProjactIoWorker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Win32;
+
+namespace ModernSort.Static
+{
+    internal static class ProjactIoWorker
+    {
+        static ProjactIoWorker()
+        {
+
+        }
+
+        internal static string FilePickerGetImage()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Выберите изображение допустимого формата";
+            ofd.Filter = "Изображения | *.jpg";
+            ofd.Multiselect = false;
+            if (ofd.ShowDialog() ?? false)
+            {
+                return ofd.FileName;
+            }
+            else 
+            {
+                throw new Exception("Не удалось выбрать файл");
+            }
+        }
+    }
+}

2acb7e5958b115f35abd372dbbc827f243d02c70
Artem Pop <vip.artem.popov.14@mail.ru>
2025-02-20 11:08:08 +0500

    Add relay command

---
+++ Commands/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernSort.Commands
+{
+    internal class RelayCommand : CommandBase
+    {
+        private event Action<object?> _action;
+        private event Func<bool> _canExecutePredicate;
+        public RelayCommand(Action<object?> action)
+        {
+            _action = action;
+        }
+        public RelayCommand(Action<object?> action, Func<bool> canExecutePredicate) : this(action)
+        {
+            _canExecutePredicate = canExecutePredicate;
+        }
+        public override void Execute(object? parameter)
+        {
+            _action?.Invoke(parameter);
+        }
+        public override bool CanExecute(object? parameter)
+        {
+            return _canExecutePredicate?.Invoke() ?? base.CanExecute(parameter);
+        }
+    }
+}

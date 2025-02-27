8b5908d7a0452c9f06bc7e3a4aa540558738ae73
Artem Pop <vip.artem.popov.14@mail.ru>
2025-02-27 21:58:44 +0500

    addNewConverters for validation messages and path to BitmapImages

---
+++ Converters/PathToBitmapImageConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace ModernSort.Converters
+{
+    internal class PathToBitmapImageConverter : IValueConverter
+    {
+        private FileStream BitmapConverterFileStream {  get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool fileNotFound = true;
+            if (value is string imagePath 
+                and not null 
+                &&  !(fileNotFound = !File.Exists(imagePath)))
+            {
+                using (BitmapConverterFileStream = File.OpenRead(imagePath))
+                {
+                    var resultBitmapImage = new BitmapImage();
+                    resultBitmapImage.BeginInit();
+                    resultBitmapImage.StreamSource = BitmapConverterFileStream;
+                    resultBitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                    resultBitmapImage.EndInit();
+                    return resultBitmapImage;
+                }
+            }
+            else
+            {
+                string ConvertProblemMessage = fileNotFound ? "not found": "incorrect format or null";
+                throw new FileNotFoundException(message: $"File {nameof(imagePath)} was {ConvertProblemMessage}");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Converters/ValidationConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ModernSort.Converters
+{
+    internal class ValidationConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values is null || values == Enumerable.Empty<object>()) 
+                throw new ArgumentException("Ошибка передачи параметров");
+
+            string result = String.Empty;
+
+            foreach (object value in values)
+            {
+                if (value is string && !value.Equals(String.Empty))
+                {
+                    result += result == String.Empty ? $"{value}": $" and {value}";
+                 }
+            }
+
+            return result;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

1c3a841fccd902182b475bcba2f0308268c63908
Artem Pop <vip.artem.popov.14@mail.ru>
2025-03-14 23:28:26 +0500

    OutputContentServices completely add

---
+++ Services/RankingContent/MediaObjectContentService.cs
@@ -0,0 +1,63 @@
+﻿using ModernSort.Stores.Catalog;
+using RankingEntityes.IO_Entities.Classes;
+using RankingEntityes.IO_Entities.Interfaces;
+using RankingEntityes.Ranking_Entityes.MediaObjacts;
+using RankingEntityes.Ranking_Entityes.Ranking_Categories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernSort.Services
+{
+    internal class MediaObjectContentService
+    {
+
+        public event Action<object?> OnMediaObjectSelectedChange;
+        private IDeserializer Deserializer { get; init; }
+        public CatalogStore CatalogStore { get; init; }
+        private MediaObject? _selectedMediaObject;
+        public MediaObject? SelectedMediaObject
+        {
+            get => _selectedMediaObject; 
+            private set
+            {
+                _selectedMediaObject = value;
+                OnMediaObjectSelectedChange?.Invoke(value);
+            }
+        }
+
+        private IoCollection<MediaObject> ExistingMediaObjects { get; set; }
+
+        public MediaObjectContentService(IDeserializer deserializer, CatalogStore catalogStore)
+        {
+            Deserializer = deserializer;
+            CatalogStore = catalogStore;
+            ExistingMediaObjects = new IoCollection<MediaObject>();
+        }
+
+        public void SelectMediaObject(MediaObject mediaObject)
+        {
+            SelectedMediaObject = ExistingMediaObjects.First(x => x.ID.Equals(mediaObject.ID)) ?? throw new ArgumentException();
+        }
+
+        public void DropSelectionOfMEdiaObject()
+        {
+            SelectedMediaObject = null;
+        }
+
+        public IoCollection<MediaObject> GetUnloadedMediaObjects()
+        {
+            ExistingMediaObjects.Deserialize(Deserializer, CatalogStore.MediaObjectsFilePath);
+            return ExistingMediaObjects;
+        }
+
+        public IEnumerable<string> GetFilesFullPathsOfSelectedMediaObject()
+        {
+            if (SelectedMediaObject is null) throw new Exception(message: "При попытке получить список полных путей всех медиа файлов заданный MediaObject был null");
+
+            return SelectedMediaObject.Paths.Select(x => CatalogStore.MediaFilesCatalogPath + "\\" + x);
+        }
+    }
+}
---
+++ Services/RankingContent/OutputContentService.cs
@@ -0,0 +1,60 @@
+﻿using ModernSort.Stores.Catalog;
+using ModernSort.ViewModel.Pages;
+using RankingEntityes.IO_Entities.Interfaces;
+using RankingEntityes.Ranking_Entityes.MediaObjacts;
+using RankingEntityes.Ranking_Entityes.Ranking_Categories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernSort.Services
+{
+    internal class OutputContentService
+    {
+        private CatalogStore CatalogStore {  get; init; }
+        private IDeserializer Deserializer { get; init; }
+        public RankingCategory? SelectedRankingCategory { get; private set; }
+
+        private MediaObjectContentService _mediaObjectContentService;
+        public MediaObjectContentService MediaObjectContentService 
+        { 
+            get 
+            {
+                return (SelectedRankingCategory is null) ? throw new Exception() : _mediaObjectContentService;
+            } 
+            init
+            {
+                _mediaObjectContentService = value;
+            } 
+        }
+
+        public OutputContentService(CatalogStore catalogStore,IDeserializer deserializer)
+        {
+            CatalogStore = catalogStore;
+            Deserializer = deserializer;
+            MediaObjectContentService = new MediaObjectContentService(Deserializer,CatalogStore);
+        }
+
+        public void SelectRankingCategory(RankingCategory rankingCategory)
+        {
+            SelectedRankingCategory = rankingCategory;
+            CatalogStore.CalibrateRankingCategory(SelectedRankingCategory);
+        }
+
+        public void DropSelectionOfRankingCategory()
+        {
+            MediaObjectContentService.DropSelectionOfMEdiaObject();
+            SelectedRankingCategory = null;
+        }
+
+        public IoCollection<RankingCategory> GetUnloadedRankingCategories()
+        {
+            IoCollection<RankingCategory> existingRankings = new IoCollection<RankingCategory>();
+            existingRankings.Deserialize(Deserializer,CatalogStore.RankingCategoriesFilePath);
+            return existingRankings;
+        }
+
+    }
+}

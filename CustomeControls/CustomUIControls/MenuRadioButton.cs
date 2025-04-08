2f2cc118cfa68a096be238af8663681e06ae5be7
Artem Pop <vip.artem.popov.14@mail.ru>
2025-04-08 09:26:23 +0500

    Createe new RadioButton custom control & new RadioButtonStyle.xaml

---
+++ CustomeControls/CustomUIControls/MenuRadioButton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace ModernSort.CustomeControls
+{
+    public class MenuRadiobutton : RadioButton
+    {
+
+        static MenuRadiobutton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MenuRadiobutton), new FrameworkPropertyMetadata(typeof(MenuRadiobutton)));
+        }
+    }
+}

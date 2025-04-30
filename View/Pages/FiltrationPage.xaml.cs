afdbce67155ece20fb9dc9e335f0af8b3593ea74
Artem Pop <vip.artem.popov.14@mail.ru>
2025-04-30 11:50:15 +0500

    FiltrationPage FilterCriterion's Filters list collapse expand graphic feature add

--- View/Pages/FiltrationPage.xaml.cs
+++ View/Pages/FiltrationPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace ModernSort.View.Pages
@@ -11,7 +12,12 @@ namespace ModernSort.View.Pages
         public FiltrationPage()
         {
             InitializeComponent();
+            ExpandCheckBoxUncheckHandler = ExpandCheckBoxClosure(Visibility.Collapsed);
+            ExpandCheckBoxCheckHandler = ExpandCheckBoxClosure(Visibility.Visible);
         }
+
+        private Action<object, RoutedEventArgs> ExpandCheckBoxUncheckHandler {  get; init; }
+        private Action<object, RoutedEventArgs> ExpandCheckBoxCheckHandler { get; init; }
 
         private void ListView_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
         {
@@ -21,6 +27,18 @@ namespace ModernSort.View.Pages
                 scrollViewer.VerticalOffset - e.Delta/3);
                 e.Handled = true;
             }
+        }
+
+        private Action<object, RoutedEventArgs> ExpandCheckBoxClosure(Visibility visibility)
+        {
+            return (object sender, RoutedEventArgs e) =>
+            {
+                if (sender is CheckBox checkBox)
+                {
+                    ListView ancestorListView = checkBox.FindName("FiltersListView") as ListView;
+                    ancestorListView.Visibility = visibility;
+                }
+            };
         }
     }
 }

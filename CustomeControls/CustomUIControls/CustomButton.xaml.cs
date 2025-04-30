d8993d09375c070a433cd91d2a141c31a5d881af
Artem Pop <vip.artem.popov.14@mail.ru>
2025-04-30 11:40:09 +0500

    Custome button add animation OnMouseEnter

--- CustomeControls/CustomUIControls/CustomButton.xaml.cs
+++ CustomeControls/CustomUIControls/CustomButton.xaml.cs
@@ -1,14 +1,18 @@
-﻿using System.IO;
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 
 namespace ModernSort.CustomeControls.CustomUIControls
 {
     /// <summary>
     /// Логика взаимодействия для CustomButton.xaml
     /// </summary>
-    public partial class CustomButton : Button
+    public partial class CustomButton : Button, INotifyPropertyChanged
     {
         public ImageSource MouseHoverImageSource { get; set; }
 
@@ -21,6 +25,12 @@ namespace ModernSort.CustomeControls.CustomUIControls
         public Thickness ImageMargin { get; set; }
 
         public CornerRadius CornerRadius { get; set; }
+
+        public Brush FirstButtonColor { get; set; }
+        public Brush SecondButtonColor { get; set; }
+
+        public Brush BorderColorBrush 
+        { get; private set; }
 
         public Thickness ImageMarginMouseHover
         {
@@ -45,10 +55,23 @@ namespace ModernSort.CustomeControls.CustomUIControls
             InitializeComponent();
         }
 
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChenged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
         public override void EndInit()
         {
             base.EndInit();
             IndexOfImageColumnSpan = (Text is not null && Text != String.Empty) ? 1 : 2;
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            BorderColorBrush = SecondButtonColor?.CloneCurrentValue() ?? (SolidColorBrush)new BrushConverter().ConvertFrom("#ffaacc");
+            OnPropertyChenged(nameof(BorderColorBrush));
         }
 
         static CustomButton()
@@ -56,5 +79,27 @@ namespace ModernSort.CustomeControls.CustomUIControls
             DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomButton), new FrameworkPropertyMetadata(typeof(CustomButton)));
         }
 
+        private void Btn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            SolidColorBrush borderColorBrush = (SolidColorBrush)BorderColorBrush;
+            SolidColorBrush firstColorBrush = (SolidColorBrush)FirstButtonColor;
+            SolidColorBrush secondColorBrush = (SolidColorBrush)SecondButtonColor;
+
+
+            var Animation = new ColorAnimation() { From = secondColorBrush.Color,To = firstColorBrush.Color, Duration = TimeSpan.FromSeconds(0.3), };
+            borderColorBrush.BeginAnimation(SolidColorBrush.ColorProperty, Animation);
+
+        }
+
+
+        private void Btn_MouseLeave(object sender, MouseEventArgs e)
+        {
+            SolidColorBrush borderColorBrush = (SolidColorBrush)BorderColorBrush;
+            SolidColorBrush firstColorBrush = (SolidColorBrush)FirstButtonColor;
+            SolidColorBrush secondColorBrush = (SolidColorBrush)SecondButtonColor;
+
+            var Animation = new ColorAnimation() { From = firstColorBrush.Color, To = secondColorBrush.Color, Duration = TimeSpan.FromSeconds(0.3), };
+            borderColorBrush.BeginAnimation(SolidColorBrush.ColorProperty, Animation);
+        }
     }
 }

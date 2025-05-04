e63ad9de854e05531bd2625bc131b19977798e85
Artem Pop <vip.artem.popov.14@mail.ru>
2025-05-04 15:31:41 +0500

    Relay command CanExecute meyhod refactoring for dynamicly update executable conditions

--- Commands/RelayCommand.cs
+++ Commands/RelayCommand.cs
@@ -1,30 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace ModernSort.Commands
 {
     internal class RelayCommand : CommandBase
     {
         private event Action<object?> _action;
         private event Func<bool> _canExecutePredicate;
+        public event Func<bool> CanExecutePredicate 
+        {
+            add
+            { 
+                _canExecutePredicate += value; 
+            }
+            remove 
+            { 
+                _canExecutePredicate -= value;
+            } 
+        }
         public RelayCommand(Action<object?> action)
         {
             _action = action;
         }
+
         public RelayCommand(Action<object?> action, Func<bool> canExecutePredicate) : this(action)
         {
-            _canExecutePredicate = canExecutePredicate;
+            CanExecutePredicate += canExecutePredicate;
         }
+
         public override void Execute(object? parameter)
         {
             _action?.Invoke(parameter);
         }
+
         public override bool CanExecute(object? parameter)
         {
-            return _canExecutePredicate?.Invoke() ?? base.CanExecute(parameter);
+            bool result = true;
+
+            if (_canExecutePredicate is null)
+                return true;
+
+            var delegates = _canExecutePredicate?.GetInvocationList();
+
+            if (delegates.Length == 1)
+                return _canExecutePredicate?.Invoke() ?? base.CanExecute(parameter);
+
+            foreach (var @delegate in delegates) 
+            {
+                result = result && (bool)@delegate.DynamicInvoke();
+            }
+
+            return result;
         }
     }
 }

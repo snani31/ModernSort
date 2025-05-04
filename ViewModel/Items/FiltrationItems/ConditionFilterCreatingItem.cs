113fa80444a64707e5c218942b441ff62417827d
Artem Pop <vip.artem.popov.14@mail.ru>
2025-05-04 15:37:19 +0500

    ConditionFilterCreatingItem VModel add

---
+++ ViewModel/Items/FiltrationItems/ConditionFilterCreatingItem.cs
@@ -0,0 +1,44 @@
+﻿using ModernSort.Commands;
+using RankingEntityes.Filters;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModernSort.ViewModel.Items.FiltrationItems
+{
+    class ConditionFilterCreatingItem: ViewModelValidateble
+    {
+        public ConditionFilter conditionFilter { get; init; }
+
+        public Func<bool> CanExecuteByValidation
+        {
+            get
+            {
+                return base.CanExecuteByValidation;
+            }
+        }
+
+        [Required(ErrorMessage = "Tytle can not be Empthy")]
+        public string ConditionTytle
+        { 
+            get 
+            { 
+                return conditionFilter.ConditionTytle;
+            }
+            set
+            {
+                conditionFilter.ConditionTytle = value;
+                Validate(nameof(ConditionTytle), value);
+            }
+        }
+
+        public ConditionFilterCreatingItem(ConditionFilter filter)
+        {
+            conditionFilter = filter;
+        }
+
+
+        public ConditionFilterCreatingItem(ConditionFilter filter, RelayCommand relayCommand) : this(filter)
+        {
+            base.PostValidationChange += relayCommand.OnCanExecuteChanged;
+        }
+    }
+}
